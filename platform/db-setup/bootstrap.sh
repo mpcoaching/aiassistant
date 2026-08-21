59c40fd80c92268d44c5bcaa2abf3b55b6621628
Martin Platt <martin.platt@builttobreaklimits.com>
2026-08-21 10:43:14 +1000

    fix: use grep -v to filter empty DB names in bootstrapper

--- platform/db-setup/bootstrap.sh
+++ platform/db-setup/bootstrap.sh
@@ -4,12 +4,7 @@ set -e
 PSQL="psql -v ON_ERROR_STOP=1 -a -e -h ${POSTGRES_HOST} -U ${POSTGRES_DB_USER} -d postgres"
 
 echo "Applying Database creation..."
-envsubst < /db-setup/001_create_databases.sql | awk '
-  /VALUES \(/ { in_values = 1 }
-  in_values && /\(\s*'\''\s*\)\s*,?\s*$/ { next }
-  { print }
-  /^[[:space:]]*\)/ { in_values = 0 }
-' | $PSQL
+envsubst < /db-setup/001_create_databases.sql | grep -v "('')" | $PSQL
 
 echo "Applying User creation..."
 envsubst < /db-setup/002_create_users.sql | $PSQL
